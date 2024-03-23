9b1fd3f2f8f5679013ac496979e0fdad0307ce48
Thierry Florac <tflorac@ulthar.net>
2024-03-23 03:10:09 +0100

    Added profile edit form content getter

--- src/pyams_zmi/zmi/profile.py
+++ src/pyams_zmi/zmi/profile.py
@@ -19,7 +19,7 @@ from zope.interface import Interface, implementer
 
 from pyams_form.ajax import AJAXFormRenderer, ajax_form_config
 from pyams_form.field import Fields
-from pyams_form.interfaces.form import IAJAXFormRenderer
+from pyams_form.interfaces.form import IAJAXFormRenderer, IFormContent
 from pyams_layer.interfaces import IPyAMSLayer
 from pyams_table.interfaces import ITable
 from pyams_utils.adapter import ContextRequestViewAdapter, adapter_config
@@ -28,9 +28,8 @@ from pyams_zmi.interfaces import IAdminLayer
 from pyams_zmi.interfaces.configuration import IZMIConfiguration
 from pyams_zmi.interfaces.form import IFormTitle
 from pyams_zmi.interfaces.profile import IUserProfile
-from pyams_zmi.interfaces.table import ITableAdminView, ITableAttributes
+from pyams_zmi.interfaces.table import ITableAttributes
 from pyams_zmi.zmi.interfaces import IUserProfileEditForm
-
 
 __docformat__ = 'restructuredtext'
 
@@ -54,14 +53,18 @@ class UserProfileEditForm(AdminModalEditForm):
             fields = fields.omit('zmi_bundle')
         return fields
 
-    def get_content(self):
-        return IUserProfile(self.request.principal)
-
     def update_widgets(self, prefix=None):
         super().update_widgets(prefix)
         bundle = self.widgets.get('zmi_bundle')
         if bundle is not None:
             bundle.no_value_message = _("Use default theme")
+
+
+@adapter_config(required=(Interface, IPyAMSLayer, UserProfileEditForm),
+                provides=IFormContent)
+def user_profile_edit_form_content(context, request, form):
+    """User profile edit form content getter"""
+    return IUserProfile(request.principal)
 
 
 @adapter_config(required=(Interface, IAdminLayer, UserProfileEditForm),
