bd8bf7157b2412e14c96a6653c880881515dedb7
Thierry Florac <tflorac@ulthar.net>
2026-06-12 22:30:40 +0200

    Added base adapters for objects label, icon and hint adapters

--- src/pyams_zmi/utils.py
+++ src/pyams_zmi/utils.py
@@ -15,14 +15,38 @@
 This module provides a small set of generic components.
 """
 
+from zope.interface import Interface
 from zope.location import ILocation
 
-from pyams_utils.adapter import adapter_config, query_adapter
+from pyams_utils.adapter import ContextRequestViewAdapter, adapter_config, query_adapter
 from pyams_utils.interfaces import MISSING_INFO
+from pyams_utils.interfaces.tales import ITALESExtension
 from pyams_zmi.interfaces import IObjectHint, IObjectIcon, IObjectLabel, IObjectName
 
 __docformat__ = 'restructuredtext'
 
+
+def get_object_adapter(interface, context, request, view=None, name=''):
+    """Get object name"""
+    adapter = query_adapter(interface, request, context, view, name)
+    if name and (adapter is None):
+        adapter = query_adapter(interface, request, context, view)
+    return adapter
+
+
+class BaseObjectAdapter(ContextRequestViewAdapter):
+    """Object name TALES extension"""
+
+    interface = None
+    
+    def render(self, context, name=''):
+        """Render TALES extension"""
+        return get_object_adapter(self.interface, context, request=self.request, view=self.view, name=name)
+
+
+#
+# Objects names
+#
 
 @adapter_config(required=ILocation,
                 provides=IObjectName)
@@ -33,11 +57,21 @@ def location_name(context):
 
 def get_object_name(context, request, view=None, name=''):
     """Get object name"""
-    adapter = query_adapter(IObjectName, request, context, view, name)
-    if name and (adapter is None):
-        adapter = query_adapter(IObjectName, request, context, view)
-    return adapter
+    return get_object_adapter(IObjectName, context, request, view, name)
 
+
+@adapter_config(name='object_name',
+                required=(Interface, Interface, Interface),
+                provides=ITALESExtension)
+class ObjectNameTalesExtension(BaseObjectAdapter):
+    """Object name TALES extension"""
+    
+    interface = IObjectName
+
+
+#
+# Objects labels
+#
 
 @adapter_config(required=ILocation,
                 provides=IObjectLabel)
@@ -48,11 +82,21 @@ def location_label(context):
 
 def get_object_label(context, request, view=None, name=''):
     """Get object label"""
-    adapter = query_adapter(IObjectLabel, request, context, view, name)
-    if name and (adapter is None):
-        adapter = query_adapter(IObjectLabel, request, context, view)
-    return adapter
+    return get_object_adapter(IObjectLabel, context, request, view, name)
 
+
+@adapter_config(name='object_label',
+                required=(Interface, Interface, Interface),
+                provides=ITALESExtension)
+class ObjectLabelTalesExtension(BaseObjectAdapter):
+    """Object label TALES extension"""
+    
+    interface = IObjectLabel
+
+
+#
+# Objects icons
+#
 
 @adapter_config(required=ILocation,
                 provides=IObjectIcon)
@@ -63,15 +107,31 @@ def location_icon(context):  # pylint: disable=unused-argument
 
 def get_object_icon(context, request, view=None, name=''):
     """Get object icon"""
-    adapter = query_adapter(IObjectIcon, request, context, view, name)
-    if name and (adapter is None):
-        adapter = query_adapter(IObjectIcon, request, context, view)
-    return adapter
+    return get_object_adapter(IObjectIcon, context, request, view, name)
 
+
+@adapter_config(name='object_icon',
+                required=(Interface, Interface, Interface),
+                provides=ITALESExtension)
+class ObjectIconTalesExtension(BaseObjectAdapter):
+    """Object icon TALES extension"""
+    
+    interface = IObjectIcon
+    
+
+#
+# Objects hints
+#
 
 def get_object_hint(context, request, view=None, name=''):
     """Get object hint"""
-    adapter = query_adapter(IObjectHint, request, context, view, name)
-    if name and (adapter is None):
-        adapter = query_adapter(IObjectHint, request, context, view)
-    return adapter
+    return get_object_adapter(IObjectHint, context, request, view, name)
+
+
+@adapter_config(name='object_hint',
+                required=(Interface, Interface, Interface),
+                provides=ITALESExtension)
+class ObjectHintTalesExtension(BaseObjectAdapter):
+    """Object hint TALES extension"""
+    
+    interface = IObjectHint
